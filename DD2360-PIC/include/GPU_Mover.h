f636474ad239918e732412f242d24995e76765cd
Soham Malhotra <sohamm@kthformulastudent.se>
2024-12-18 16:38:02 +0100

    added streams, changed xi[2] to xi and so on, moved some values outside kernel

--- DD2360-PIC/include/GPU_Mover.h
+++ DD2360-PIC/include/GPU_Mover.h
@@ -5,4 +5,5 @@
 
 void gpu_mover_PC(struct GPUParticles** gpu_part, struct GPUEMfield* gpu_field, struct GPUGrid* gpu_grd, struct particles** part, struct parameters* param);
 
-__global__ void mover_PC_kernel(struct GPUParticles* gpu_part, struct GPUEMfield* gpu_field, struct GPUGrid* gpu_grd, __grid_constant__ const struct parameters param);
+__global__ void mover_PC_kernel(struct GPUParticles* gpu_part, struct GPUEMfield* gpu_field, struct GPUGrid* gpu_grd, __grid_constant__ const struct parameters param, 
+                                FPpart dto2, FPpart qomdt2, FPpart dt_sub_cycling);
