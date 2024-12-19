590cc5259627e4a5bd5ed91063876cbab4db5afe
David Szekedi <D.Szekedi@student.tudelft.nl>
2024-12-19 10:39:00 +0100

    trying something different

--- DD2360-PIC/src/GPU_Interpolator.cu
+++ DD2360-PIC/src/GPU_Interpolator.cu
@@ -1,7 +1,7 @@
 #include "GPU_Interpolator.h"
 #include <assert.h>
 
-#define THREAD_NR 128
+#define THREAD_NR 128  // multiple of 32
 #define PART_SIZE_TEMP 128
 
 void gpu_interpP2G(struct GPUParticles** gpu_part, struct GPUInterpDensSpecies** gpu_ids, struct GPUGrid* gpu_grd, struct particles** part, struct grid* grd, struct parameters* param) {
@@ -17,20 +17,90 @@ void gpu_interpP2G(struct GPUParticles** gpu_part, struct GPUInterpDensSpecies**
     }
 }
 
-__global__ void interpP2G_kernel(struct GPUParticles* gpu_part, struct GPUInterpDensSpecies* gpu_ids, struct GPUGrid* gpu_grd) {  // TODO constant memory!
+// __global__ void interpP2G_kernel(struct GPUParticles* gpu_part, struct GPUInterpDensSpecies* gpu_ids, struct GPUGrid* gpu_grd) {  // TODO constant memory!
 
-    FPpart weight;
+//     __shared__ FPfield cell_rho[8], cell_Jx[8], cell_Jy[8], cell_Jz[8];
+//     __shared__ FPfield XN[2], YN[2], ZN[2];
+//     __shared__ FPpart x[THREAD_NR], y[THREAD_NR], z[THREAD_NR], u[THREAD_NR], v[THREAD_NR], w[THREAD_NR];
+//     __shared__ FPfield xi[THREAD_NR][2], eta[THREAD_NR][2], zeta[THREAD_NR][2];
 
-    __shared__ FPfield cell_vals_arr[32]; // 8 rho, 8 Jx, 8 Jy, 8 Jz
-    __shared__ FPfield XN[2], YN[2], ZN[2];
-    __shared__ FPpart x[THREAD_NR], y[THREAD_NR], z[THREAD_NR], u[THREAD_NR], v[THREAD_NR], w[THREAD_NR];
-    __shared__ FPfield xi[THREAD_NR][2], eta[THREAD_NR][2], zeta[THREAD_NR][2];
+//     if (threadIdx.x < 8) {
+//         cell_rho[threadIdx.x] = 0;
+//         cell_Jx[threadIdx.x] = 0;
+//         cell_Jy[threadIdx.x] = 0;
+//         cell_Jz[threadIdx.x] = 0;
+//     }
 
-    if (threadIdx.x < 32) {  // first 8 threads zero shared memory
-        cell_vals_arr[threadIdx.x] = 0;
-    }
+//     __syncthreads();
 
-    __syncthreads();
+//     if (threadIdx.x < 6) {  // first 6 threads load cell coordinates
+//         FPfield* N;
+//         FPfield* N_GPU_GLAT;
+    
+//         if (threadIdx.x / 2 == 0) {  // these are just pointers!
+//             N = XN;
+//             N_GPU_GLAT = gpu_grd->XN_GPU_flat;
+//         } else if (threadIdx.x / 2 == 1) {
+//             N = YN;
+//             N_GPU_GLAT = gpu_grd->YN_GPU_flat;
+//         } else {
+//             N = ZN;
+//             N_GPU_GLAT = gpu_grd->ZN_GPU_flat;
+//         }
+        
+//         N[threadIdx.x % 2] = N_GPU_GLAT[(blockIdx.x + threadIdx.x % 2 + 1) * gpu_grd->nzn * gpu_grd->nyn + 
+//                         (blockIdx.y + threadIdx.x % 2 + 1) * gpu_grd->nzn + 
+//                         (blockIdx.z + threadIdx.x % 2 + 1)];
+//     }
+
+//     __syncthreads();
+
+//     for(long part_ind = threadIdx.x; part_ind < PART_SIZE_TEMP; part_ind += blockDim.x) {
+//           // TODO this is a disaster, use some sort of data structure
+//         if (true) {  //(gpu_part->cell_id[part_ind] == (blockIdx.x + 1) * gpu_grd->nzc * gpu_grd->nyc + (blockIdx.y + 1) * gpu_grd->nzc + (blockIdx.z + 1)) {
+
+//             FPpart weight;
+            
+//             x[threadIdx.x] = gpu_part->x[part_ind];
+//             y[threadIdx.x] = gpu_part->y[part_ind];
+//             z[threadIdx.x] = gpu_part->z[part_ind];
+//             u[threadIdx.x] = gpu_part->u[part_ind];
+//             v[threadIdx.x] = gpu_part->v[part_ind];
+//             w[threadIdx.x] = gpu_part->w[part_ind];
+
+//             xi[threadIdx.x][0] = x[threadIdx.x] - XN[0];
+//             eta[threadIdx.x][0] = y[threadIdx.x] - YN[0];
+//             zeta[threadIdx.x][0] = z[threadIdx.x] - ZN[0];
+//             xi[threadIdx.x][1] = XN[1] - x[threadIdx.x];
+//             eta[threadIdx.x][1] = YN[1] - y[threadIdx.x];
+//             zeta[threadIdx.x][1] = ZN[1] - z[threadIdx.x];
+
+//             weight = gpu_part->q[part_ind] * gpu_grd -> invVOL * gpu_grd -> invVOL;  // saved into register to avoid repeated access
+
+//             for(int i = 0; i < 2; i++)
+//                 for(int j = 0; j < 2; j++)
+//                     for(int k = 0; k < 2; k++) {
+//                         atomicAdd(&(cell_rho[i * 4 + j * 2 + k]), weight * xi[threadIdx.x][i] * eta[threadIdx.x][j] * zeta[threadIdx.x][k]);
+//                         atomicAdd(&(cell_Jx[i * 4 + j * 2 + k]), weight * xi[threadIdx.x][i] * eta[threadIdx.x][j] * zeta[threadIdx.x][k] * u[threadIdx.x]);
+//                         atomicAdd(&(cell_Jy[i * 4 + j * 2 + k]), weight * xi[threadIdx.x][i] * eta[threadIdx.x][j] * zeta[threadIdx.x][k] * v[threadIdx.x]);
+//                         atomicAdd(&(cell_Jz[i * 4 + j * 2 + k]), weight * xi[threadIdx.x][i] * eta[threadIdx.x][j] * zeta[threadIdx.x][k] * w[threadIdx.x]);
+//                     }
+//         }
+//     }
+
+//     __syncthreads();
+
+//     if(threadIdx.x < 8) {  // fist 8 threads transfer shared memory to global
+//         atomicAdd(&(gpu_ids->rhon_flat[((blockIdx.x + 2) - threadIdx.x / 4) * gpu_grd->nzn * gpu_grd->nyn + ((blockIdx.y + 2) - (threadIdx.x % 4) / 2) * gpu_grd->nzn + ((blockIdx.z + 2) - threadIdx.x % 2)]), cell_rho[threadIdx.x]);
+//         atomicAdd(&(gpu_ids->Jx_flat[((blockIdx.x + 2) - threadIdx.x / 4) * gpu_grd->nzn * gpu_grd->nyn + ((blockIdx.y + 2) - (threadIdx.x % 4) / 2) * gpu_grd->nzn + ((blockIdx.z + 2) - threadIdx.x % 2)]), cell_Jx[threadIdx.x]);
+//         atomicAdd(&(gpu_ids->Jy_flat[((blockIdx.x + 2) - threadIdx.x / 4) * gpu_grd->nzn * gpu_grd->nyn + ((blockIdx.y + 2) - (threadIdx.x % 4) / 2) * gpu_grd->nzn + ((blockIdx.z + 2) - threadIdx.x % 2)]), cell_Jy[threadIdx.x]);
+//         atomicAdd(&(gpu_ids->Jz_flat[((blockIdx.x + 2) - threadIdx.x / 4) * gpu_grd->nzn * gpu_grd->nyn + ((blockIdx.y + 2) - (threadIdx.x % 4) / 2) * gpu_grd->nzn + ((blockIdx.z + 2) - threadIdx.x % 2)]), cell_Jz[threadIdx.x]);
+//     }
+// }
+
+__global__ void interpP2G_kernel(struct GPUParticles* gpu_part, struct GPUInterpDensSpecies* gpu_ids, struct GPUGrid* gpu_grd) {
+
+    __shared__ FPfield XN[2], YN[2], ZN[2];  // let's have each thread individually load its particles for now
 
     if (threadIdx.x < 6) {  // first 6 threads load cell coordinates
         FPfield* N;
@@ -52,55 +122,42 @@ __global__ void interpP2G_kernel(struct GPUParticles* gpu_part, struct GPUInterp
                         (blockIdx.z + threadIdx.x % 2 + 1)];
     }
 
-    __syncthreads();
+    FPfield accumulateRho = 0;
+    FPfield accumulateJx = 0;
+    FPfield accumulateJy = 0;
+    FPfield accumulateJz = 0;
 
-    for(long part_ind = threadIdx.x; part_ind < PART_SIZE_TEMP; part_ind += blockDim.x) {
-          // TODO this is a disaster, use some sort of data structure
-        if (true) {  //(gpu_part->cell_id[part_ind] == (blockIdx.x + 1) * gpu_grd->nzc * gpu_grd->nyc + (blockIdx.y + 1) * gpu_grd->nzc + (blockIdx.z + 1)) {
-            
-            x[threadIdx.x] = gpu_part->x[part_ind];
-            y[threadIdx.x] = gpu_part->y[part_ind];
-            z[threadIdx.x] = gpu_part->z[part_ind];
-            u[threadIdx.x] = gpu_part->u[part_ind];
-            v[threadIdx.x] = gpu_part->v[part_ind];
-            w[threadIdx.x] = gpu_part->w[part_ind];
+    int warpId = threadIdx.x / 32;
+    int numWarps = blockDim.x / 32;
+    int quantId = (threadIdx.x % 32) / 8;
+    int cellId = threadIdx.x % 8;
 
-            xi[threadIdx.x][0] = x[threadIdx.x] - XN[0];
-            eta[threadIdx.x][0] = y[threadIdx.x] - YN[0];
-            zeta[threadIdx.x][0] = z[threadIdx.x] - ZN[0];
-            xi[threadIdx.x][1] = XN[1] - x[threadIdx.x];
-            eta[threadIdx.x][1] = YN[1] - y[threadIdx.x];
-            zeta[threadIdx.x][1] = ZN[1] - z[threadIdx.x];
+    printf("Thread %d, warp %d, quant %d, cell %d\n", threadIdx.x, warpId, quantId, cellId);
 
-            weight = gpu_part->q[part_ind] * gpu_grd -> invVOL * gpu_grd -> invVOL;  // saved into register to avoid repeated access
+    int i = cellId / 4;  // 0 or 1
+    int j = (cellId % 4) / 2;   // 0 or 1
+    int k = cellId % 2;  // 0 or 1
 
-            int laneId = threadIdx.x % 32;
+    int warpPartSize = PART_SIZE_TEMP / numWarps;
 
-            int subWarpId = laneId / 8;
-            int subLaneId = laneId % 8;
+    for(long part_ind = warpId * warpPartSize; part_ind < (warpId + 1) * warpPartSize; part_ind++) {
+        // TODO iterate from first to last particle in current block
 
-            for(int i = 0; i < 2; i++)
-                for(int j = 0; j < 2; j++)
-                    for(int k = 0; k < 2; k++) {
-                        // atomicAdd(&(cell_rho[i * 4 + j * 2 + k]), weight * xi[threadIdx.x][i] * eta[threadIdx.x][j] * zeta[threadIdx.x][k]);  // TODO thread has its own counter?
-                        // atomicAdd(&(cell_Jx[i * 4 + j * 2 + k]), weight * xi[threadIdx.x][i] * eta[threadIdx.x][j] * zeta[threadIdx.x][k] * u[threadIdx.x]);
-                        // atomicAdd(&(cell_Jy[i * 4 + j * 2 + k]), weight * xi[threadIdx.x][i] * eta[threadIdx.x][j] * zeta[threadIdx.x][k] * v[threadIdx.x]);
-                        // atomicAdd(&(cell_Jz[i * 4 + j * 2 + k]), weight * xi[threadIdx.x][i] * eta[threadIdx.x][j] * zeta[threadIdx.x][k] * w[threadIdx.x]);
+        FPfield vels[4]; // shared?
+        vels[0] = 1;
+        vels[1] = gpu_part->u[part_ind];
+        vels[2] = gpu_part->v[part_ind];
+        vels[3] = gpu_part->w[part_ind];
 
-                        cell_rho[i * 4 + j * 2 + k] += weight * xi[threadIdx.x][i] * eta[threadIdx.x][j] * zeta[threadIdx.x][k];
-                        cell_Jx[i * 4 + j * 2 + k] += weight * xi[threadIdx.x][i] * eta[threadIdx.x][j] * zeta[threadIdx.x][k] * u[threadIdx.x];
-                        cell_Jy[i * 4 + j * 2 + k] += weight * xi[threadIdx.x][i] * eta[threadIdx.x][j] * zeta[threadIdx.x][k] * v[threadIdx.x];
-                        cell_Jz[i * 4 + j * 2 + k] += weight * xi[threadIdx.x][i] * eta[threadIdx.x][j] * zeta[threadIdx.x][k] * w[threadIdx.x];
-                    }
-        }
+        accumulate += gpu_part->q[part_ind] * gpu_grd -> invVOL * gpu_grd -> invVOL * vels[quantId] * (gpu_part->x[part_ind] - XN[i]) * (gpu_part->y[part_ind] - YN[j]) * (gpu_part->z[part_ind] - ZN[k]);  // TODO signs!
+
     }
 
-    __syncthreads();
+    FPfield* quants[4];  // shared?
+    quants[0] = &(gpu_ids->rhon_flat[((blockIdx.x + 2) - i) * gpu_grd->nzn * gpu_grd->nyn + ((blockIdx.y + 2) - j) * gpu_grd->nzn + ((blockIdx.z + 2) - k)]);
+    quants[1] = &(gpu_ids->Jx_flat[((blockIdx.x + 2) - i) * gpu_grd->nzn * gpu_grd->nyn + ((blockIdx.y + 2) - j) * gpu_grd->nzn + ((blockIdx.z + 2) - k)]);
+    quants[2] = &(gpu_ids->Jy_flat[((blockIdx.x + 2) - i) * gpu_grd->nzn * gpu_grd->nyn + ((blockIdx.y + 2) - j) * gpu_grd->nzn + ((blockIdx.z + 2) - k)]);
+    quants[3] = &(gpu_ids->Jz_flat[((blockIdx.x + 2) - i) * gpu_grd->nzn * gpu_grd->nyn + ((blockIdx.y + 2) - j) * gpu_grd->nzn + ((blockIdx.z + 2) - k)]);
 
-    if(threadIdx.x < 8) {  // fist 8 threads transfer shared memory to global
-        atomicAdd(&(gpu_ids->rhon_flat[((blockIdx.x + 2) - threadIdx.x / 4) * gpu_grd->nzn * gpu_grd->nyn + ((blockIdx.y + 2) - (threadIdx.x % 4) / 2) * gpu_grd->nzn + ((blockIdx.z + 2) - threadIdx.x % 2)]), cell_rho[threadIdx.x]);
-        atomicAdd(&(gpu_ids->Jx_flat[((blockIdx.x + 2) - threadIdx.x / 4) * gpu_grd->nzn * gpu_grd->nyn + ((blockIdx.y + 2) - (threadIdx.x % 4) / 2) * gpu_grd->nzn + ((blockIdx.z + 2) - threadIdx.x % 2)]), cell_Jx[threadIdx.x]);
-        atomicAdd(&(gpu_ids->Jy_flat[((blockIdx.x + 2) - threadIdx.x / 4) * gpu_grd->nzn * gpu_grd->nyn + ((blockIdx.y + 2) - (threadIdx.x % 4) / 2) * gpu_grd->nzn + ((blockIdx.z + 2) - threadIdx.x % 2)]), cell_Jy[threadIdx.x]);
-        atomicAdd(&(gpu_ids->Jz_flat[((blockIdx.x + 2) - threadIdx.x / 4) * gpu_grd->nzn * gpu_grd->nyn + ((blockIdx.y + 2) - (threadIdx.x % 4) / 2) * gpu_grd->nzn + ((blockIdx.z + 2) - threadIdx.x % 2)]), cell_Jz[threadIdx.x]);
-    }
+    atomicAdd(quants[quantId], accumulate);
 }
